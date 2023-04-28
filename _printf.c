4dd795282230ace9f3b356cb0085ba07965dc57b
Mejdoubee <recovery.mejdoub@gmail.com>
2023-04-28 22:10:53 +0100

    putchar

--- _printf.c
+++ _printf.c
@@ -10,9 +10,13 @@ int _printf(const char *format, ...)
 {
 	va_list args;
 	int total = 0;
+	handleWith_t *handles;
+
 
 	if (format == NULL)
+	{
 		return (-1);
+	}
 	va_start(args, format);
 	while (*format)
 	{
@@ -24,29 +28,33 @@ int _printf(const char *format, ...)
 				va_end(args);
 				return (-1);
 			}
-			else if (*format == 'c')
-				total += handleWith_c(args);
-			else if (*format == 's')
-				total += handleWith_s(args);
-			else if (*format == 'i' || *format == 'd')
-				total += handleWith_d_i(args);
-			else if (*format == 'u')
-				total += handleWith_u(args);
-			else if (*format == 'o')
-				total += handleWith_o(args);
-			else if (*format == 'x')
-				total += handleWith_x(args);
-			else if (*format == 'X')
-				total += handleWith_X(args);
-			else if (*format == 'b')
-				total += handleWith_b(args);
-			else if (*format == '%')
-				_putchar('%'), total++;
+			if (*format == '%')
+			{
+				_putchar('%');
+				total++;
+			}
 			else
-				_putchar('%'), _putchar(*format), total += 2;
+			{
+			handles = array;
+			while (handles->handled_format)
+			{
+					if (handles->handled_format == *format)
+					{
+						total += handles->handledfunction(args);
+						break;
+					}
+					handles++;
+				}
+				if (handles->handled_format == '\0')
+				{
+					_putchar('%'), _putchar(*format), total += 2;
+				}
+			}
 		}
 		else
+		{
 			_putchar(*format), total++;
+		}
 		format++;
 	}
 	va_end(args);
