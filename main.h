713c2e56eef5150a015b651d12bf766590fa4416
Mejdoubee <recovery.mejdoub@gmail.com>
2023-04-28 14:21:27 +0100

    original

--- main.h
+++ main.h
@@ -2,6 +2,23 @@
 #define MAIN_H
 
 #include <stdarg.h>
+#include <stddef.h>
+#include <limits.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+/**
+* struct handleWith - Structure for handling format specifiers
+* @hand: Format specifier character
+* @f: Function pointer to the corresponding handler function for the format specifier
+*
+*/
+typedef struct handleWith
+{
+	char hand;
+	int (*f)(va_list args);
+} handleWith_t;
+
 
 int _printf(const char *format, ...);
 
